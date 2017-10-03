c3426a705a186376b2d711ef1f11e69117abffc4
root <root@neil-X553MA>
2017-10-03 20:49:09 +0200

    Temperature Adruino Project 4 led's

---
+++ LM35_Sencor_Project.ino
@@ -0,0 +1,54 @@
+#include "Arduino.h"
+//The setup function is called once at startup of the sketch
+int pot		=	0;
+int reading	=	0;
+int	tempC	=	0;
+int	pin13	=	13;
+int pin12	=	12;
+int pin11	=	11;
+int pin10	=	10;
+void setup()
+{
+	Serial.begin(9600);
+	analogReference(INTERNAL);
+	pinMode(pin13,OUTPUT);
+	pinMode(pin12,OUTPUT);
+	pinMode(pin11,OUTPUT);
+	pinMode(pin10,OUTPUT);
+// Add your initialization code here
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+	reading	=	analogRead(pot);
+	tempC	=	reading / 9.31l;
+	Serial.println(tempC);
+	delay(1000);
+	if(tempC <=10){
+		digitalWrite(pin10, HIGH);
+		delay(1000);
+		digitalWrite(pin10, LOW);
+		delay(1000);
+	}else if (tempC >10 && tempC <=20) {
+		digitalWrite(pin11, HIGH);
+		delay(1000);
+		digitalWrite(pin11, LOW);
+		delay(1000);
+
+	}else if (tempC >20 && tempC <=30) {
+		digitalWrite(pin12, HIGH);
+		delay(1000);
+		digitalWrite(pin12, LOW);
+		delay(1000);
+
+	}else if (tempC >30 && tempC <=40) {
+		digitalWrite(pin13, HIGH);
+		delay(1000);
+		digitalWrite(pin13, LOW);
+		delay(1000);
+
+	}
+
+//Add your repeated code here
+}
---
+++ Release/.ino.cpp.d
@@ -0,0 +1,37 @@
+.ino.cpp.o: ../.ino.cpp \
+ /home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Arduino.h \
+ /home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/binary.h \
+ /home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/WCharacter.h \
+ /home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/WString.h \
+ /home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/HardwareSerial.h \
+ /home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Stream.h \
+ /home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Print.h \
+ /home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Printable.h \
+ /home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/USBAPI.h \
+ /home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Arduino.h \
+ /home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/variants/standard/pins_arduino.h \
+ ../LM35_Sencor_Project.ino
+
+/home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Arduino.h:
+
+/home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/binary.h:
+
+/home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/WCharacter.h:
+
+/home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/WString.h:
+
+/home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/HardwareSerial.h:
+
+/home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Stream.h:
+
+/home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Print.h:
+
+/home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Printable.h:
+
+/home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/USBAPI.h:
+
+/home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/cores/arduino/Arduino.h:
+
+/home/neil/eclipse/cpp-oxygen/eclipse/arduinoPlugin/packages/arduino/hardware/avr/1.6.20/variants/standard/pins_arduino.h:
+
+../LM35_Sencor_Project.ino:
