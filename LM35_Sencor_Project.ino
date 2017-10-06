a9fb118904e414eac6eb8cd5a7965fc671652092
root <root@neil-X553MA>
2017-10-06 18:32:55 +0200

    Pet o meter fixed

--- LM35_Sencor_Project.ino
+++ LM35_Sencor_Project.ino
@@ -1,8 +1,10 @@
 #include "Arduino.h"
 
 int pot = 0;
+int pot1	=	5;
+int value	=	0;
 int reading = 0;
-int tempC = 0;
+float tempC = 0;
 int pin13 = 13;
 int pin12 = 12;
 int pin11 = 11;
@@ -18,29 +20,37 @@ void setup() {
 }
 
 void loop() {
+
 	reading = analogRead(pot);
-	tempC = reading / 9.31l;
+	tempC = reading /9.31;
+	value	=	analogRead(A5);
 	Serial.println(tempC);
-	delay(1000);
+
 		if (tempC <= 10) {
 			digitalWrite(pin10, HIGH);
-			delay(1000);
+			delay(value);
 			digitalWrite(pin10, LOW);
-			delay(1000);
+			delay(value);
+
 		} else if (tempC > 10 && tempC <= 20) {
 			digitalWrite(pin11, HIGH);
-			delay(1000);
+			delay(value);
 			digitalWrite(pin11, LOW);
-			delay(1000);
+			delay(value);
+
+
 		} else if (tempC > 20 && tempC <= 30) {
 			digitalWrite(pin12, HIGH);
-			delay(1000);
+			delay(value);
 			digitalWrite(pin12, LOW);
-			delay(1000);
+			delay(value);
+
 		} else if (tempC > 30 && tempC <= 40) {
 			digitalWrite(pin13, HIGH);
-			delay(1000);
+			delay(value);
 			digitalWrite(pin13, LOW);
-			delay(1000);
+			delay(value);
+
+
 		}
 }
